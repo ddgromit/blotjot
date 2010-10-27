f266b36cb03072f39d07f5e183ddd240660972d2
ddgromit <derekdahmer@gmail.com>
2010-10-27 16:54:25 -0400

    Homepage functionality

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -9,8 +9,23 @@ function randomString(string_length) {
 }
 
 $(function() {
+    // Select the URL
     $("input[name='url_suffix']")
         .val(randomString(6))
         .focus().select();
+
+    // Form submission
+    $(".ipad-size").click(function() {
+        goBoard('ipad');
+    });
+    $(".iphone-size").click(function() {
+        goBoard('iphone');
+    });
+    function goBoard(type) {
+        $(".size").attr("disabled","disabled");
+        var room_id = $("input[name='url_suffix']").val();
+
+        window.location = '/create?type=' + type + '&room_id=' + room_id;
+    }
 });
 
